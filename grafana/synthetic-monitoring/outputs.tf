82670cba03ea8a3586394f7cc57dc8aae766a218
Greg Sharpe! <git+me@gregsharpe.co.uk>
2025-02-17 20:10:19 +0000

    Updating the total calculation of frequency to use the grafana_synthetic_check

--- grafana/synthetic-monitoring/outputs.tf
+++ grafana/synthetic-monitoring/outputs.tf
@@ -3,9 +3,8 @@ output "grafna_probes_locations" {
 }
 
 # According to Grafana Synthetic Monitoring pricing, the free tier includes 100,000 executions per month. This output should calculate the total usage of the different checks against the amount of free evaluators granted.
-
 locals {
-  total_frequency_seconds = sum([for k, v in var.endpoints : v.frequency]) / 1000
+  total_frequency_seconds = sum([for _, v in grafana_synthetic_monitoring_check.synthetic_monitoring_checks : v.frequency ])
   seconds_per_month       = 30 * 24 * 60 * 60
   total_checks_allowed    = 100000
   total_possible_checks   = local.seconds_per_month / local.total_frequency_seconds
@@ -13,7 +12,7 @@ locals {
 }
 
 output "probe_total_frequency" {
-  value = sum([for k, v in var.endpoints : v.frequency])
+  value = local.total_frequency_seconds
 }
 
 output "total_possible_checks" {
--- grafana/synthetic-monitoring/variables.tf
+++ grafana/synthetic-monitoring/variables.tf
@@ -1,16 +1,16 @@
 variable "endpoints" {
   description = "A map of endpoints to create synthetic monitoring checks for."
-  type        = map(object({
-    frequency = optional(number)
-    job       = optional(string)
-    target    = string
-    settings  = optional(
-      map(
-        object({
-          http = map(any)
-        }
-      )
-    )
-  )
-  }))
+  # type        = map(object({
+  #   frequency = optional(number)
+  #   job       = optional(string)
+  #   target    = string
+  #   settings  = optional(
+  #     map(
+  #       object({
+  #         http = map(any)
+  #       }
+  #     )
+  #   )
+  # )
+  # }))
 }
