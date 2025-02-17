61ac890bc25e60d9c3fee065dc05dd24b2ea75b0
Greg Sharpe! <git+me@gregsharpe.co.uk>
2025-02-17 20:21:07 +0000

    Updating

--- grafana/synthetic-monitoring/outputs.tf
+++ grafana/synthetic-monitoring/outputs.tf
@@ -4,7 +4,7 @@ output "grafna_probes_locations" {
 
 # According to Grafana Synthetic Monitoring pricing, the free tier includes 100,000 executions per month. This output should calculate the total usage of the different checks against the amount of free evaluators granted.
 locals {
-  total_frequency_seconds = sum([for _, v in grafana_synthetic_monitoring_check.synthetic_monitoring_checks : v.frequency ])
+  total_frequency_seconds = sum([for _, v in grafana_synthetic_monitoring_check.synthetic_monitoring_checks : v.frequency ]) / 1000
   seconds_per_month       = 30 * 24 * 60 * 60
   total_checks_allowed    = 100000
   total_possible_checks   = local.seconds_per_month / local.total_frequency_seconds
