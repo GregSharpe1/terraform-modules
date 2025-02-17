f91351dc35b776af2ad067eea5d93d8245d39d1b
Greg Sharpe! <git+me@gregsharpe.co.uk>
2025-02-17 19:52:38 +0000

    Making the variable endpoints, optional

--- grafana/synthetic-monitoring/variables.tf
+++ grafana/synthetic-monitoring/variables.tf
@@ -1,12 +1,16 @@
 variable "endpoints" {
   description = "A map of endpoints to create synthetic monitoring checks for."
   type        = map(object({
-    frequency = number
-    job       = string
+    frequency = optional(number)
+    job       = optional(string)
     target    = string
-    # TODO (gsharpe) - Add support for other job types and settings. (User Journey type checks)
-    # settings  = map(object({
-    #   http = map(any)
-    # }))
+    settings  = optional(
+      map(
+        object({
+          http = map(any)
+        }
+      )
+    )
+  )
   }))
 }
