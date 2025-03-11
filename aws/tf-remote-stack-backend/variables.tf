d0f692d885776824f73b063eb77fbc2322acd3d8
Greg Sharpe! <git+me@gregsharpe.co.uk>
2025-03-11 18:43:32 +0000

    Adding a region for the aws variable

--- aws/tf-remote-stack-backend/variables.tf
+++ aws/tf-remote-stack-backend/variables.tf
@@ -1,5 +1,10 @@
 # variables.tf
 
+variable "region" {
+  description = "AWS region to deploy resources"
+  type        = string
+}
+
 variable "bucket_name" {
   description = "Name of the S3 bucket for Terraform state"
   type        = string
